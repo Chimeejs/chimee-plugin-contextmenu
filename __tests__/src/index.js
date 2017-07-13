366754b8b95b77d2e018ffc908a6d72184a8ff1b
huzunjie <huzunjie@360.cn>
2017-07-13 15:10:41 +0800

    first commit

---
+++ __tests__/src/index.js
@@ -0,0 +1,142 @@
+import chimeeContextmenu from 'index';
+import {$} from 'chimee-helper';
+
+// 模拟构造
+const $domWrap = $('<chimee-menu>').append($('<ul></ul>'));
+const $dom = $domWrap[0];
+Object.assign(
+  chimeeContextmenu,
+  {
+    $domWrap,
+    $dom,
+    $config: {
+    },
+    _emit: {},
+    $emit (act) {
+      this._emit[act] = (this._emit[act] || 0) + 1;
+    },
+    $bumpToTop () {}
+  },
+  chimeeContextmenu.methods,
+  chimeeContextmenu.data
+);
+
+test('create', () => {
+  chimeeContextmenu.create();
+
+  expect(chimeeContextmenu.name).toBe('chimeeContextmenu');
+
+  const baseMenusCount = chimeeContextmenu.data.baseMenus.length;
+  expect(chimeeContextmenu.baseMenus).toHaveLength(baseMenusCount);
+  expect($domWrap.find('li')).toHaveLength(baseMenusCount);
+
+  chimeeContextmenu.$config.baseMenus = chimeeContextmenu.baseMenus.concat([{
+    text: 'txt_base', action: 'txt_base'
+  }]);
+  chimeeContextmenu.create();
+  expect($domWrap.find('li[data-action="txt_base"]')).toHaveLength(1);
+});
+
+test('clickHandler', done => {
+  // 测试菜单点击事件绑定
+  const _clickHandler = chimeeContextmenu.clickHandler;
+  chimeeContextmenu.clickHandler = function (e) {
+    expect(e.target.tagName).toBe('LI');
+    done();
+  };
+  chimeeContextmenu.create();
+  $domWrap.find('li')[0].click();
+  chimeeContextmenu.clickHandler = _clickHandler;
+
+  // 菜单项 disable 状态
+  chimeeContextmenu.clickHandler({
+    target: {
+      disable: true
+    },
+    stopPropagation () {}
+  });
+
+  // 模拟正常状态
+  let _act_run = 0;
+  chimeeContextmenu.openLogPopup = e => {
+    _act_run = 1;
+  };
+  let _stopPropagation_run = 0;
+  chimeeContextmenu.clickHandler({
+    target: {
+      disable: false,
+      dataset: {
+        action: 'openLogPopup'
+      }
+    },
+    stopPropagation () {
+      _stopPropagation_run++;
+    }
+  });
+  expect(chimeeContextmenu._emit.openLogPopup).toBe(1);
+  expect(_act_run).toBe(1);
+  expect(_stopPropagation_run).toBe(1);
+
+  // action对应 method 不存在的情况
+  chimeeContextmenu.clickHandler({
+    target: {
+      disable: false,
+      dataset: {
+        action: undefined
+      }
+    },
+    stopPropagation () {
+      _stopPropagation_run++;
+    }
+  });
+  expect(_stopPropagation_run).toBe(2);
+});
+
+test('updatemenu', () => {
+  chimeeContextmenu.updatemenu([
+    { text: 'txt1', action: 'act1' },
+    { text: 'txt2', action: 'act2' }
+  ]);
+  expect(chimeeContextmenu.$domWrap.find('[data-action="act1"]')).toHaveLength(1);
+  expect(chimeeContextmenu.$domWrap.find('[data-action="act2"]')).toHaveLength(1);
+});
+
+test('switchLogMenu', () => {
+  // “查看日志”菜单项显示隐藏
+  chimeeContextmenu.events.logPluginDestroy.call(chimeeContextmenu);
+  expect(chimeeContextmenu.$domWrap.find('[data-action="openLogPopup"]').css('display')).toBe('none');
+  chimeeContextmenu.events.logPluginCreate.call(chimeeContextmenu);
+  expect(chimeeContextmenu.$domWrap.find('[data-action="openLogPopup"]').css('display')).not.toBe('none');
+
+  // “查看日志”菜单项不存在的情况
+  chimeeContextmenu.baseMenus = [];
+  chimeeContextmenu.updatemenu([]);
+  chimeeContextmenu.switchLogMenu(true);
+  expect(chimeeContextmenu.$domWrap.find('[data-action="openLogPopup"]')).toHaveLength(0);
+  chimeeContextmenu.baseMenus = chimeeContextmenu.data.baseMenus;
+});
+
+test('events.mousedown', () => {
+  chimeeContextmenu.open();
+  expect(chimeeContextmenu._hide).toBe(false);
+  chimeeContextmenu.events.mousedown.call(chimeeContextmenu, { button: 2 });
+  expect(chimeeContextmenu._hide).toBe(false);
+  chimeeContextmenu.events.mousedown.call(chimeeContextmenu, { button: 1 });
+  expect(chimeeContextmenu._hide).toBe(true);
+});
+
+test('events.contextmenu', () => {
+  /* 
+  this.offset(e.offsetX + 'px ' + e.offsetY + 'px').open(e);
+  e.preventDefault();
+  */
+  expect(chimeeContextmenu._hide).toBe(true);
+  let _stop_run = 0;
+  chimeeContextmenu.events.contextmenu.call(chimeeContextmenu, {
+    offsetX: 0,
+    offsetY: 0,
+    preventDefault () { _stop_run++; }
+  });
+  expect(_stop_run).toBe(1);
+  expect(chimeeContextmenu._hide).toBe(false);
+});
---
+++ src/index.js
@@ -0,0 +1,102 @@
+import popupFactory from 'chimee-plugin-popup';
+import {isFunction, isArray} from 'chimee-helper';
+import './index.css';
+const logPopupOpenEvt = 'openLogPopup';
+/* 右键菜单插件
+ * 接收参数：baseMenus, menus
+ * 也可通过方法 updatemenu 更新自定义菜单项
+ */
+const chimeeContextmenu = popupFactory({
+  tagName: 'chimee-menu',
+  name: 'chimeeContextmenu',
+  html: '<ul></ul>',
+  offset: '0',
+  hide: true,
+  data: {
+    // 默认菜单项
+    baseMenus: [
+      {
+        text: '查看日志',
+        action: logPopupOpenEvt,
+        hide: true
+      },
+      {
+        text: 'copyright 360 vs: {VERSION}',
+        disable: true
+      },
+      {
+        text: '关闭',
+        className: '_close'
+      }
+    ]
+  },
+  create () {
+    const {baseMenus, menus} = this.$config;
+    if(isArray(baseMenus)) {
+      this.baseMenus = baseMenus;
+    }
+    this.updatemenu(menus);
+    this.$domWrap.on('click', this.clickHandler);
+  },
+  opened () {
+    // 盖住所有插件
+    this.$bumpToTop();
+  },
+  events: {
+    contextmenu (e) {
+      this.offset(e.offsetX + 'px ' + e.offsetY + 'px').open(e);
+      e.preventDefault();
+    },
+    mousedown (e) {
+      e.button !== 2 && this.close(e);
+    },
+    // 当日志插件卸载时隐藏“查看日志”菜单项
+    logPluginDestroy () {
+      this.switchLogMenu(true);
+    },
+    // 当日志插件状态完毕显示“查看日志”菜单项
+    logPluginCreate () {
+      this.switchLogMenu(false);
+    }
+  },
+  methods: {
+    switchLogMenu (hide) {
+      const logMenuConf = this.baseMenus.find(item => item.action === logPopupOpenEvt);
+      if(logMenuConf) {
+        logMenuConf.hide = hide;
+        this.$domWrap.find('[data-action="' + logPopupOpenEvt + '"]').css('display', hide ? 'none' : '');
+      }
+    },
+    updatemenu (menusConfig) {
+      let menus = [];
+      if (isArray(menusConfig)) {
+        menus = menus.concat(menusConfig);
+      }
+      // console.log(this.$plugins.chimeeLog);
+      const meunsHTML = menus.concat(this.baseMenus).map(({hide, disable, action, className, text}) => `
+        <li
+         ${disable ? ' disable' : ''}
+         ${action ? ' data-action="' + action + '"' : ''}
+         ${className ? ' class="' + className + '"' : ''}
+         ${hide ? ' style="display:none"' : ''}
+        >${text.replace(/\{([^)]*)\}/g, (_, matchStr) => this[matchStr])}</li>
+      `).join('');
+      this.$domWrap.find('ul').html(meunsHTML);
+    },
+    clickHandler (e) {
+      const el = e.target;
+      if (!el.disable) {
+        const act = el.dataset.action;
+        if (act) {
+          // 抛出右键项点击对应事件给全局
+          this.$emit(act);
+          isFunction(this[act]) && this[act](e);
+        }
+        this.close();
+      }
+      e.stopPropagation();
+    }
+  }
+});
+
+module.exports = chimeeContextmenu;
